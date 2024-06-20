fe633dea5861d4247a4a260ed6677bcc54577b04
nikhilmalhotra007 <22bsp70047@cuchd.in>
2024-06-20 11:24:05 +0530

    improve look of signup and login page with remove login bugs

--- scripts/login.script.js
+++ scripts/login.script.js
@@ -1,46 +1,17 @@
-import { USER_LOGIN_URL} from "../config.js";
+import { USER_LOGIN_URL } from "../config.js";
 import { setCookieWithExpirationInSeconds } from "./utils.script.js";
-
+import { toggleDisplay } from "./utils.script.js";
 const form = document.getElementById("form");
-const regBtn = document.getElementById("reg-btn") 
+const regBtn = document.getElementById("reg-btn");
 
-regBtn.addEventListener("click",()=>{
-  window.location.href = "./signup.html"
-})
+regBtn.addEventListener("click", () => {
+  window.location.href = "./signup.html";
+});
 
+const postLoginRequest = async (username, password) => {
+  const URL = USER_LOGIN_URL;
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const loaderDiv = document.getElementById("loader");
-  const submitbtn = document.getElementById("submit");
-
-  const refreshOnError = (refresh) => {
-    if (refresh) {
-      loaderDiv.classList.remove("loader-visible");
-      submitbtn.disabled = false;
-      submitbtn.style.background =
-        "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)";
-      location.reload();
-      return;
-    }
-
-    loaderDiv.classList.add("loader-visible");
-    submitbtn.disabled = true;
-    submitbtn.style.background =
-      "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,0.4515056022408963) 56%, rgba(252,176,69,1) 100%)";
-  };
-
-  
-
-  refreshOnError(false)
-
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-
-  const URL = USER_LOGIN_URL
-
-  fetch(URL, {
+  const res = await fetch(URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -49,42 +20,165 @@ form.addEventListener("submit", (e) => {
       username,
       password,
     }),
-  })
-    .then((res) => {
+  }).then((res) => {
+    console.log(res.status);
+    if (res.status == 400) {
+      handleResponseOnError("username or email is required");
+      return null;
+    } else if (res.status == 404) {
+      handleResponseOnError("user doesn't exist");
+      return null;
+    } else if (res.status == 401) {
+      handleResponseOnError("Invalid password");
+      return null;
+    } else if (res.status == 500) {
+      handleResponseOnError("something went wrong while loging");
+      return null;
+    } else if (res.status == 200) {
+      return res.json();
+    }
+  });
 
-      if (res.status == 400) {
-        alert("username or email is required");
-        refreshOnError(true)
-      } 
-      if (res.status == 404) {
-        alert("user doesn't exist");
-        refreshOnError(true)
-      } 
-      else if (res.status == 401) {
-        alert("Invalid password");
-        refreshOnError(true)
-      } 
-      else if (res.status == 500) {
-        alert("something went wrong while loging");
-        refreshOnError(true)
-        
-      } 
+  console.log(res);
 
-      return res.json()
-    })
-    .then(res =>{
-       loaderDiv.classList.remove("loader-visible");
-       setCookieWithExpirationInSeconds("accessToken",res.data.accessToken,parseInt(res.data.expiry))
-       setCookieWithExpirationInSeconds("id",res.data.user._id,parseInt(res.data.expiry))
-       setCookieWithExpirationInSeconds("username",res.data.user.username,parseInt(res.data.expiry))
-       setCookieWithExpirationInSeconds("avatar",res.data.user.avatar,parseInt(res.data.expiry))
-       document.location.href = "../pages/home.html"
-       return
-    })
-    .catch((err) => {
-        
-      console.log(err);
-    });
-});
+  return res ? res : null;
+};
 
+const renderLoadingScreen = (render) => {
+  const loaderDiv = document.getElementById("loader");
+  toggleDisplay(loaderDiv, render);
+};
 
+const renderSubmitBtn = (isDisable, backgroundColor) => {
+  const submitbtn = document.getElementById("submit");
+
+  submitbtn.disabled = isDisable;
+  submitbtn.style.background = backgroundColor;
+};
+
+const handleResponseOnError = (message) => {
+  alert(message);
+  renderLoadingScreen(false);
+  renderSubmitBtn(false, "#22577a");
+  location.reload();
+};
+
+const handleResponseOnSuccess = (data) => {
+  setCookieWithExpirationInSeconds(
+    "accessToken",
+    data.accessToken,
+    parseInt(data.expiry)
+  );
+  setCookieWithExpirationInSeconds("id", data.user._id, parseInt(data.expiry));
+  setCookieWithExpirationInSeconds(
+    "username",
+    data.user.username,
+    parseInt(data.expiry)
+  );
+  setCookieWithExpirationInSeconds(
+    "avatar",
+    data.user.avatar,
+    parseInt(data.expiry)
+  );
+  document.location.href = "../pages/home.html";
+  return;
+};
+
+const getFormData = () => {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  return { username, password };
+};
+
+const handleLoginForm = async (event) => {
+  event.preventDefault();
+  renderLoadingScreen(true);
+  renderSubmitBtn(true, "#0084db");
+
+  const { username, password } = getFormData();
+
+  const res = await postLoginRequest(username, password);
+
+  if (res) {
+    handleResponseOnSuccess(res.data());
+  }
+};
+
+form.addEventListener("submit", handleLoginForm);
+
+//   {
+//   e.preventDefault();
+
+//   const loaderDiv = document.getElementById("loader");
+//   const submitbtn = document.getElementById("submit");
+
+//   const refreshOnError = (refresh) => {
+//     if (refresh) {
+//       loaderDiv.classList.remove("loader-visible");
+//       submitbtn.disabled = false;
+//       submitbtn.style.background =
+//         "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)";
+//       location.reload();
+//       return;
+//     }
+
+//     loaderDiv.classList.add("loader-visible");
+//     submitbtn.disabled = true;
+//     submitbtn.style.background =
+//       "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,0.4515056022408963) 56%, rgba(252,176,69,1) 100%)";
+//   };
+
+//   refreshOnError(false)
+
+//   const username = document.getElementById("username").value;
+//   const password = document.getElementById("password").value;
+
+//   const URL = USER_LOGIN_URL
+
+//   fetch(URL, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       username,
+//       password,
+//     }),
+//   })
+//     .then((res) => {
+
+//       if (res.status == 400) {
+//         alert("username or email is required");
+//         refreshOnError(true)
+//       }
+//       if (res.status == 404) {
+//         alert("user doesn't exist");
+//         refreshOnError(true)
+//       }
+//       else if (res.status == 401) {
+//         alert("Invalid password");
+//         refreshOnError(true)
+//       }
+//       else if (res.status == 500) {
+//         alert("something went wrong while loging");
+//         refreshOnError(true)
+
+//       }
+
+//       return res.json()
+//     })
+//     .then(res =>{
+//        loaderDiv.classList.remove("loader-visible");
+//        setCookieWithExpirationInSeconds("accessToken",res.data.accessToken,parseInt(res.data.expiry))
+//        setCookieWithExpirationInSeconds("id",res.data.user._id,parseInt(res.data.expiry))
+//        setCookieWithExpirationInSeconds("username",res.data.user.username,parseInt(res.data.expiry))
+//        setCookieWithExpirationInSeconds("avatar",res.data.user.avatar,parseInt(res.data.expiry))
+//        document.location.href = "../pages/home.html"
+//        return
+//     })
+//     .catch((err) => {
+
+//       console.log(err);
+//     });
+// }
--- scripts/signup.script.js
+++ scripts/signup.script.js
@@ -1,32 +1,89 @@
-import { USER_URL,POST_URL } from "../config.js";
+import { USER_URL } from "../config.js";
+import {toggleDisplay} from "../scripts/utils.script.js";
 
-const form = document.getElementById("form");
+const postFormData = async (formData) => {
+  const URL = USER_URL + "auth/signup";
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+  const res = await fetch(URL, {
+    method: "POST",
+    body: formData,
+  })
+    .then((res) => {
+      return res;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
+    return res
+};
+
+const renderLoadingScreen = (render) => {
   const loaderDiv = document.getElementById("loader");
-  loaderDiv.classList.toggle("loader-visible");
+  toggleDisplay(loaderDiv, render);
+};
 
+const renderSubmitBtn = (isDisable, backgroundColor) => {
   const submitbtn = document.getElementById("submit");
 
-  submitbtn.disabled = true;
-  submitbtn.style.background =
-    "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,0.4515056022408963) 56%, rgba(252,176,69,1) 100%)";
+  submitbtn.disabled = isDisable;
+  submitbtn.style.background = backgroundColor;
+};
+
+const getFormData = () => {
   const username = document.getElementById("username").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  var gender = document.getElementsByName("gender");
+  let gender = document.getElementsByName("gender");
 
   for (let i = 0; i < gender.length; i++) {
     if (gender[i].checked) gender = gender[i].value;
   }
   const file = document.getElementById("avatar")?.files[0];
 
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return { username, email, password, gender, file };
+};
 
-  if (!emailRegex.test(email)) {
+const validateEmail = (email) => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+};
+
+const handleResponse = (res) =>{
+
+  if(res.status == 409){
+    alert("user already exist with username or email!");
+    renderLoadingScreen(false)
+    renderSubmitBtn(false,"#22577a")
+    location.reload();
+  }
+
+  else if (res.status == 500) {
+    alert("something went wrong while signing-up");
+    renderLoadingScreen(false)
+    renderSubmitBtn(false,"#22577a")
+    location.reload();
+  }
+
+  else if(res.status == 201){
+    window.location.href = "./login.html";
+  }
+}
+
+const handleForm = async (event) => {
+  event.preventDefault();
+
+  renderLoadingScreen(true);
+  renderSubmitBtn(true, "#0084db");
+
+  const { username, email, password, gender, file } = getFormData();
+
+  if (!validateEmail(email)) {
     alert("invalid email");
+    renderLoadingScreen(false);
+    renderSubmitBtn(false, "#22577a");
+    location.reload()
+    return;
   }
 
   const newFormData = new FormData();
@@ -39,39 +96,17 @@ form.addEventListener("submit", (e) => {
     newFormData.append("avatar", file);
   }
 
-  const URL =  USER_URL + "auth/signup"
+  const res = await postFormData(newFormData)
 
-  fetch(URL, {
-    method: "POST",
-    body: newFormData,
-  })
-    .then((res) => {
+  handleResponse(res)
+};
 
-      if (res.status == 409) {
-        alert("user already exist with email or username");
-        submitbtn.disabled = false;
-        submitbtn.style.background =
-          "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)";
+const form = document.getElementById("form");
 
-        location.reload()
-        return;
-      } 
-      else if (res.status == 500){
-        alert("something went wrong while signing-up");
-        submitbtn.disabled = false;
-        submitbtn.style.background =
-          "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)";
-        loaderDiv.classList.toggle("loader-visible");
-        location.reload()
-        return;
-      }
-      else {
-        res = res.json();
-        loaderDiv.classList.toggle("loader-visible");
-        window.location.href = "./login.html";
-        return
-      }
-    })
-    .catch((error) => {
-    });
-});
+form.addEventListener("submit", handleForm)
+  
+export {
+  renderLoadingScreen,
+  renderSubmitBtn,
+  validateEmail
+}
